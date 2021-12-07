2184525ecebc236d99b99296eeb72412f77c3d3f
Marina Schramm Jorge Schneider <84358232+marinasjs@users.noreply.github.com>
2021-12-07 10:16:23 -0300

    Add files via upload

---
+++ Ps2a5.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ps2a5;
+
+/**
+ *
+ * @author marina
+ */
+public class Ps2a5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaContador tc = new TelaContador();
+        tc.setVisible(true);
+    }
+    
+}
